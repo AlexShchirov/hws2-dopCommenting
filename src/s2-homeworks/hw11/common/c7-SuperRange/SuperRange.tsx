80e3278ece8b7cdb2ddd09a8552fea910b4e74ed
Alexander Shchirov <shchirov1998@mail.ru>
2024-06-10 21:09:58 +0300

    hw11finref

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,30 +1,43 @@
 import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
+import styled from '@emotion/styled'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
-        <Slider
+        <MySlider
             sx={{ // стили для слайдера // пишет студент
-                color: '#64fe76', // цвет заполненной части слайдера
-                '& .MuiSlider-thumb': {
-                    height: 24, // высота ползунка
-                    width: 24, // ширина ползунка
-                    backgroundColor: '#ffffff', // цвет ползунка
-                    border: '2px solid currentColor', // граница ползунка
-                    '&:focus, &:hover, &.Mui-active': {
-                        boxShadow: 'inherit', // тень ползунка при фокусе, наведении и активации
-                    },
-                },
-                '& .MuiSlider-rail': {
-                    color: '#E6E6E6', // цвет незаполненной части слайдера
-                },
-                '& .MuiSlider-track': {
-                    border: 'none', // убираем границу заполненной части слайдера
-                },
+
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
 }
+
+const MySlider = styled(Slider)({
+    color: '#00CC22',
+    height: 4,
+    width: '200px',
+
+    '& .MuiSlider-track': {
+      border: 'none',
+    },
+    '& .MuiSlider-thumb': {
+      height: 24,
+      width: 24,
+      backgroundColor: '#fff',
+      border: '2px solid currentColor',
+      '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+        boxShadow: 'inherit',
+      },
+      '&::before': {
+        display: 'none',
+      },
+    },
+    '& .MuiSlider-rail': {
+        opacity: 0.5,
+        boxShadow: 'inset 0px 0px 4px -2px #000',
+        backgroundColor: '#d0d0d0',
+    },
+  });
 
 export default SuperRange
